b7979ae4d00dea1ddd00b01f06b8ab4a882434c5
Vitor <vitorhoesouza@gmail.com>
2021-08-26 11:53:05 -0300

    yes

---
+++ api_tutorial/models/user.js
@@ -0,0 +1,11 @@
+//Banco de dados
+
+const users = [
+    { id: 1, name: "Pedro", email: "pedro@email.com" },
+    { id: 2, name: "João", email: "joao@email.com" },
+    { id: 3, name: "Marcos", email: "marcos@email.com" },
+];
+
+module.exports = [
+    users
+]

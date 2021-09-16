2c2ae38e9a8b63ee6a124732534d2275ad9f47ba
Vitor <vitorhoesouza@gmail.com>
2021-09-16 11:56:23 -0300

    err

--- Aula 61/react-navigation/screens/DrawerScreens/UseReducerScreen.js
+++ Aula 61/react-navigation/screens/DrawerScreens/UseReducerScreen.js
@@ -1,32 +1,12 @@
-import React, { useReducer } from 'react'
-import { Text, View, TextInput } from "react-native";
-import { UseReducerScreen } from '../StackScreens/UseReducerScreen';
+import React from "react";
+import { View } from "react-native";
+import { UseReducerScreen } from "../StackScreens/UseReducerScreen"
 
+export default function App() {
 
-export function UseReducerScreen(state, action) {
-    const [state, dispatch] = useReducer(reducer, { first: '', last: '' })
-  
-    return (
-      <View>
-        <TextInput
-          style={{ fontSize: 32 }}
-          placeholder="First"
-          value={state.first}
-          onChangeText={(text) => {
-            dispatch({ type: 'first', value: text })
-          }}
-        />
-        <TextInput
-          style={{ fontSize: 32 }}
-          placeholder="Last"
-          value={state.last}
-          onChangeText={(text) => {
-            dispatch({ type: 'last', value: text })
-          }}
-        />
-        <Text style={{ fontSize: 32 }}>
-          Hello {state.first} {state.last}
-        </Text>
-      </View>
-    )
-  }
+  return (
+    <View>
+      <UseReducerScreen />
+    </View>
+  );
+}
